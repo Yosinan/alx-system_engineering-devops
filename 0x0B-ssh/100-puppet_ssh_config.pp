eb19ec619e68e023aacf4a357f35024aedca71ba
Yosinan <ymymym4634@gmial.com>
2023-03-25 04:53:32 -0700

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Client configuration file (w/ Puppet)
+# this script will set up your client SSH configuration file so that you can connect to a server without typing a password
+
+file_line {  'pwd_no_autentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line {  'pwd_direction':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
