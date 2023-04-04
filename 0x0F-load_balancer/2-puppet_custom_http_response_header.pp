c70b4ecf813a2ae51d6cf4e567acf930b5be7497
Yosinan <ymymym4634@gmial.com>
2023-04-04 11:00:54 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,28 +2,23 @@
 exec { 'update':
   command => '/usr/bin/env apt-get update -y',
 }
-
-package { 'nginx':
+-> package { 'nginx':
   ensure  => installed,
   require => Exec['update'],
 }
-
 # the header
-
-file_line { 'cust_header':
+-> file_line { 'cust_header':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
-  after   => 'serrver_name _:',
+  after   => 'server_name _:',
  # target => '/etc/nginx/nginx.conf',
-  line    => "\t add_header X-Served-By ${hostname};",
+  line    => "http \n\t add_header X-Served-By \"${hostname}\";",
   require => package['nginx'],
 }
-
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
-
-service { 'nginx':
+-> service { 'nginx':
   ensure  => running,
   require => File_line['cust_header'],
   command => 'usr/sbin/ service nginx restart',}
