4b93affe3ee0d23577fc6ca9b735dda4b43ce9fc
Yosinan <ymymym4634@gmial.com>
2023-04-05 01:47:00 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,16 @@
 # this will automate tge previous task i've done( creating custom header for the http response)
 
 exec { 'update':
-  command => '/usr/bin/apt-get update -y',
+  command => '/usr/bin/apt-get update',
 }
 -> package { 'nginx':
   ensure  => 'present',
 }
 # the header
 -> file_line { 'http_header':
-  path => '/etc/nginx/nginx.conf',
+  path  => '/etc/nginx/nginx.conf',
   match => 'http {',
-  line   => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 -> exec { 'run':
   #require => File_line['cust_header'],
