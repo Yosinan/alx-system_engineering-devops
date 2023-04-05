e9c33ebd8d860bad426379b76b514cccb1da57a9
Yosinan <ymymym4634@gmial.com>
2023-04-04 23:33:09 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,7 +8,7 @@ exec { 'update':
 # the header
 -> file_line { 'cust_header':
   target => '/etc/nginx/nginx.conf',
-  line   => "http \n\t add_header X-Served-By \"${hostname}\";",
+  line   => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 -> exec { 'run':
   #require => File_line['cust_header'],
