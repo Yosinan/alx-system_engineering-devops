5d2ca8488023437e59dc791813be7522496bec53
Yosinan <ymymym4634@gmial.com>
2023-04-08 12:40:21 -0700

    Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,19 +3,34 @@
 exec { 'update':
   command => 'sudo apt-get -y update',
 }
--> package { 'nginx':
+
+package { 'nginx':
   ensure  => 'present',
-}
--> exec { 'nginx install':
-  provider => shell,
-  command => 'sudo apt-get install nginx -y',
-}
--> exec { 'add_header':
-  path  => '/etc/nginx/nginx.conf',
-  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$hostname\";/" /etc/nginx/nginx.conf',
+  require => Exec['update'],
 }
 
--> exec { 'run':
-  
-  command => 'sudo service nginx restart',
+file_line { '404':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'add_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
