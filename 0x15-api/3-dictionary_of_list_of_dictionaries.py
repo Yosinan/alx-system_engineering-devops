cd53b37985c64dd354f9ef80f9d5c095aef51977
Yosinan <ymymym4634@gmail.com>
2023-04-24 12:33:24 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,53 +2,44 @@
 '''
 an API that returns information about his/her TODO list progress.
 '''
+import json
 import requests
 import sys
 
-empApi = 'https://jsonplaceholder.typicode.com/users/{}'
-todoApi = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+empApi = 'https://jsonplaceholder.typicode.com/users/'
 
 
-def get_employee_name(emp_id):
+def get_employees():
     '''
-    returns employee name based on the id it receives
+    returns all employees
     '''
-    res_employee = requests.get(empApi.format(emp_id)).json()
-    return res_employee['name']
+    res_employee = requests.get(empApi).json()
+    return res_employee
 
 
-def get_todo_list(emp_id):
+def all_to_JSON(employee):
     '''
-    returns the TODO lists of the specific employee based on the ID
+    convert the todos of the employee into JSON file
     '''
-    res_todo = requests.get(todoApi.format(emp_id)).json()
-
-    return res_todo
+    address = 'https://jsonplaceholder.typicode.com/'
+    res = {}
+    for emp in employee:
+        user_id = emp.get('id')
+        todos = requests.get(
+            address + 'todos?userId={}'.format(user_id)).json()
+        res[user_id] = []
+        for todo in todos:
+            row = {'username': emp.get('username'), 'task': todo.get('title'),
+                   'completed': todo.get('completed')}
+            res[user_id].append(row)
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(res, f)
 
 
 if __name__ == '__main__':
     '''
-    if the specified argument's length not equal to 2 it will give an errror
-    '''
-    if len(sys.argv) != 2:
-        print('Usage: python todo.py <employee_id>')
-        sys.exit(1)
+    calls the functions that will export data in the JSON format.
 
-    emp_id = sys.argv[1]
-    if not emp_id.isdigit():
-        print('please enter a no for the employee id\n')
-        sys.exit(1)
-    employeeName = get_employee_name(emp_id)
-    todo_list = get_todo_list(emp_id)
-
-    total_tasks = len(todo_list)
-    completed_tasks = sum(1 for task in todo_list if task['completed'])
-
-    print('Employee {} is done with tasks({}/{}):'.format(employeeName,
-          completed_tasks, total_tasks))
     '''
-    lists the completed tasks of a specific employee
-    '''
-    for task in todo_list:
-        if task['completed']:
-            print('{} {}'.format('\t', task['title']))
+    emp = get_employees()
+    all_to_JSON(emp)
