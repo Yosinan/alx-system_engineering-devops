e894724de204ed15aef3e9802d2187bd317ea8c1
Yosinan <ymymym4634@gmail.com>
2023-05-09 09:46:53 -0700

    web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix the issue of Apache returning a 500 error
 
 exec { 'solve-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   provider => shell,
 }
